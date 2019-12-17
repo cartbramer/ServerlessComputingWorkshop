12afc8c1a8c1857b677dc32295fcd8b965658e61
bartcramer <bart.cramer@gmail.com>
2019-12-17 14:57:38 +0100

    First working version of computing primes in Function App.

---
+++ ComputePrimesFunctionApp/ComputePrimesHttpTrigger/__init__.py
@@ -0,0 +1,18 @@
+import logging
+import azure.functions as func
+
+from . import compute_primes
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    m1 = int(req.params.get('m1'))
+    m2 = int(req.params.get('m2'))
+
+    if not m1 or not m2 : 
+        return func.HttpResponse(
+             "Not a well-formed query",
+             status_code=400)
+
+    return func.HttpResponse("The number of primes: " + str(compute_primes.how_many_primes_between(m1, m2)))
